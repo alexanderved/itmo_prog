0741c42829c92264f04f501ca35c2c08aedfb3e7
alexanderved <avedehin692@gmail.com>
2024-11-07 17:10:29 +0300

    Update lab2/{compile.sh,run.sh}

--- lab2/compile.sh
+++ lab2/compile.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
+rm -rf build
+mkdir -p build/classes
+
 javac -cp "lib/*:src/" -d build/classes src/proglab2/Main.java
 jar -c -f build/lab2.jar -m META-INF/MANIFEST.MF -C build/classes/ .
--- lab2/run.sh
+++ lab2/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
+mkdir -p build/lib
+cp lib/Pokemon.jar build/lib/
 java -jar build/lab2.jar
